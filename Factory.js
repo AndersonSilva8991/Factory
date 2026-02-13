7addb5da0616c07655bec97de6e9c6e3b1ed022b
Anderson Soares da Silva <115432955+AndersonSilva8991@users.noreply.github.com>
2026-02-13 11:41:37 -0300

    Add files via upload

---
+++ Factory.js
@@ -0,0 +1,9 @@
+function createstory(title, author) {
+    const story = {
+        storyTitle: title,
+        storyAuthor: author
+    }
+    return story
+}
+const story1 = createstory("Matrix", "Lilly and Lana")
+console.log(story1)
